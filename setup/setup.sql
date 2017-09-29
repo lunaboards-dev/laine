8b0153c7a43a063adead3b61903f5543ea9c7c2b
Adorable-Catgirl <janeroxanne0@gmail.com>
2017-09-29 16:47:39 -0400

    added mustache templating for the setup program

--- setup/setup.sql
+++ setup/setup.sql
@@ -1,26 +1,26 @@
-DROP DATABASE laine;
-CREATE DATABASE laine;
-USE laine;
+CREATE DATABASE `{{dbname}}` DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+USE {{dbname}};
 CREATE TABLE posts (
-	date bigint,
-	board text,
-	id bigint,
-	post text
-);
+	date bigint NOT NULL,
+	board text NOT NULL,
+	id bigint NOT NULL,
+	post text NOT NULL
+) ENGINE = InnoDB;
 CREATE TABLE threads (
-	board text,
-	name text,
-	id biginit,
-	ip text,
-	lastupdate bigint,
-	locked tinyint(1),
-	pinned tinyint(1),
-	marked tinyint(1)
-);
+	board text NOT NULL,
+	name text NOT NULL,
+	id bigint NOT NULL,
+	ip text NOT NULL,
+	lastupdate bigint NOT NULL,
+	locked BOOLEAN NOT NULL,
+	pinned BOOLEAN NOT NULL,
+	marked BOOLEAN NOT NULL
+) ENGINE = InnoDB;
 CREATE TABLE admins (
-	name text,
-	perm text,
-	phash text,
-	boardperm text,
-	k text
-);
+	name text NOT NULL,
+	perm text NOT NULL,
+	phash text NOT NULL,
+	boardperm text NOT NULL,
+	k text NOT NULL
+) ENGINE = InnoDB;
+GRANT ALL PRIVILEGES ON {{dbname}}.* TO '{{user}}'@'%' IDENTIFIED BY '{{pass}}';
